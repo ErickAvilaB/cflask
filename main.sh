dc1f2e4cea13495e0b9c93fcd936ac3e59440b1b
Erick Avila <yahir7083@gmail.com>
2022-05-13 14:57:25 -0500

    Upload to GitHub

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "[+] --- Flask projects's name: ---"
+read name
+mkdir $name
+cd $name
+echo [+] --- Directory $name created ---
+
+python3 -m venv venv
+echo [+] --- Virtual Enviroment initialized ---
+
+. venv/bin/activate
+echo [+] --- Virtual Enviroment activated ---
+
+pip install Flask
+echo [+] --- Flask installed ---
+
+cp -r ~/workspace/CreateFlaskProject/files/src ./
+echo [+] --- Files has reated ---
+ls | batcat --theme=TwoDark
+tree src/ | batcat --theme=TwoDark
+
+echo [+] ----- Project $name created -----
